3027b6e02ab6096d34d1c389d9c918240376c2be
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-03-02 21:19:28 +0200

    adding some hardcoded data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular 6';
+  
+  public todos: Todo[] = [
+    {id: 1, title: 'Call mom', completed: false},
+    {id: 2, title: 'Cook a dinner', completed: false},
+    {id: 3, title: 'Learn spanish', completed: false},
+  ]
+}
+
+export interface Todo {
+  id: number
+  title: string
+  completed: boolean
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from '../app.component'
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  @Input() todos: Todo[]
 
   constructor() { }
 
